dc7db6f3a64ecfc4338e9f158a2fe2137e7d15ef
Thomas <mthomas.pro@outlook.fr>
2023-04-18 16:19:31 +0200

    v0.2.0
    - creation of the responsive login page
    - adding the page in the router
    - login form creation

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -2,6 +2,8 @@ import { Link } from 'react-router-dom';
 import { Logo } from '../components/Logo';
 
 export function Header() {
+    const connected = true;
+
     return (
         <header className='header'>
             <nav className='header_navigation'>
@@ -9,9 +11,50 @@ export function Header() {
                     <Logo />
                 </Link>
                 <ul>
-                    <li>
-                        <Link to='/'>&gt; Sign In</Link>
-                    </li>
+                    {connected ? (
+                        <>
+                            <li>
+                                <Link to='/profile'>
+                                    <svg
+                                        xmlns='http://www.w3.org/2000/svg'
+                                        width='24'
+                                        height='24'
+                                        viewBox='0 0 24 24'
+                                    >
+                                        <path d='M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm0 22c-3.123 0-5.914-1.441-7.749-3.69.259-.588.783-.995 1.867-1.246 2.244-.518 4.459-.981 3.393-2.945-3.155-5.82-.899-9.119 2.489-9.119 3.322 0 5.634 3.177 2.489 9.119-1.035 1.952 1.1 2.416 3.393 2.945 1.082.25 1.61.655 1.871 1.241-1.836 2.253-4.628 3.695-7.753 3.695z' />
+                                    </svg>
+                                    <p>Jean</p>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to='/signin'>
+                                    <svg
+                                        xmlns='http://www.w3.org/2000/svg'
+                                        width='24'
+                                        height='24'
+                                        viewBox='0 0 24 24'
+                                    >
+                                        <path d='M16 9v-4l8 7-8 7v-4h-8v-6h8zm-16-7v20h14v-2h-12v-16h12v-2h-14z' />
+                                    </svg>
+                                    <p>Sign Out</p>
+                                </Link>
+                            </li>
+                        </>
+                    ) : (
+                        <li>
+                            <Link to='/signin'>
+                                <svg
+                                    xmlns='http://www.w3.org/2000/svg'
+                                    width='24'
+                                    height='24'
+                                    viewBox='0 0 24 24'
+                                >
+                                    <path d='M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm0 22c-3.123 0-5.914-1.441-7.749-3.69.259-.588.783-.995 1.867-1.246 2.244-.518 4.459-.981 3.393-2.945-3.155-5.82-.899-9.119 2.489-9.119 3.322 0 5.634 3.177 2.489 9.119-1.035 1.952 1.1 2.416 3.393 2.945 1.082.25 1.61.655 1.871 1.241-1.836 2.253-4.628 3.695-7.753 3.695z' />
+                                </svg>
+                                <p>Sign In</p>
+                            </Link>
+                        </li>
+                    )}
                 </ul>
             </nav>
         </header>
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { Home } from '../pages/home/Home';
+import { Connection } from '../pages/connection/Connection';
 
 export function Router() {
     return (
         <Routes>
             <Route path='/' element={<Home />} />
+            <Route path='/signin' element={<Connection />} />
             <Route path='*' element={<Navigate replace to='/' />} />
         </Routes>
     );
