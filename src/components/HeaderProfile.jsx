59e952f6c52360df1cc2cd1b0a8cac5c7514a493
Thomas <mthomas.pro@outlook.fr>
2023-05-03 15:42:28 +0200

    v0.6.3
    - creation of a Profile header component for the profile page header

---
+++ src/components/HeaderProfile.jsx
@@ -0,0 +1,100 @@
+import { useState } from 'react';
+import { handleName } from '../states/userSlice';
+import { useDispatch } from 'react-redux';
+
+export default function EditNameForm({ firstName, lastName, token }) {
+    const [toggleEditBtn, setToggleEditBtn] = useState(false);
+    const [errorName, setErrorName] = useState(false);
+    const dispatch = useDispatch();
+    var regex = /^[a-zA-Z ]{2,30}$/;
+
+    function handleEditBtn(event) {
+        event.preventDefault();
+        setToggleEditBtn(!toggleEditBtn);
+    }
+
+    async function handleUserName(event) {
+        event.preventDefault();
+
+        const userInfos = {
+            firstName: event.target[0].value
+                ? event.target[0].value
+                : firstName,
+            lastName: event.target[1].value ? event.target[1].value : lastName,
+        };
+
+        if (regex.test(userInfos.firstName) && regex.test(userInfos.lastName)) {
+            await fetch('http://localhost:3001/api/v1/user/profile', {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                    authorization: `Bearer ${token}`,
+                },
+                body: JSON.stringify(userInfos),
+            })
+                .then((res) => res.json())
+                .then((body) => {
+                    if (body.body) {
+                        dispatch(handleName(userInfos));
+                        setToggleEditBtn(!toggleEditBtn);
+                        setErrorName(false);
+                    }
+                })
+                .catch((err) => console.log(err));
+        } else {
+            setErrorName(true);
+        }
+    }
+
+    return (
+        <div className='profile_header'>
+            <h1>Welcome back</h1>
+            {!toggleEditBtn ? (
+                <>
+                    <p>
+                        {firstName} {lastName} !
+                    </p>
+                    <button className='green_btn' onClick={handleEditBtn}>
+                        Edit name
+                    </button>
+                </>
+            ) : (
+                <>
+                    <form
+                        className='profile_header_form'
+                        onSubmit={handleUserName}
+                    >
+                        <div className='profile_header_form_input'>
+                            <input
+                                type='text'
+                                name='firstName'
+                                placeholder={firstName}
+                            />
+                            <input
+                                type='text'
+                                name='lastName'
+                                placeholder={lastName}
+                            />
+                        </div>
+                        {errorName && (
+                            <p className='error'>
+                                Incorrect first or last name
+                            </p>
+                        )}
+                        <div className='profile_header_form_btn'>
+                            <button className='green_btn' type='submit'>
+                                Save
+                            </button>
+                            <button
+                                className='green_btn'
+                                onClick={handleEditBtn}
+                            >
+                                Cancel
+                            </button>
+                        </div>
+                    </form>
+                </>
+            )}
+        </div>
+    );
+}
